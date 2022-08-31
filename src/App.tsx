91ab7cc8411e74cc184d490fde523e857cc8b931
BarBronshtein <barbronshtein@gmail.com>
2022-08-31 19:41:38 +0300

    added routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import './assets/scss/styles.scss';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 declare const require: any;
 function App() {
 	const logo = require('./logo.svg') as string;
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+				</header>
+			</Routes>
 		</div>
 	);
 }
