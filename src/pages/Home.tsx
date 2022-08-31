5f8d9dff6b6a42fbca3d3ffce6b828f04c06ee38
BarBronshtein <barbronshtein@gmail.com>
2022-08-31 20:58:54 +0300

    added home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import Loading from '../components/Loading';
+import StoryList from '../components/Story-list';
+import { Story } from '../models';
+import { storyService } from '../services/story.service';
 
 const Home = () => {
-	return <div></div>;
+	const [stories, setStories] = useState<null | Story[]>(null);
+	const getStories = useCallback(async () => {
+		const stories = await storyService.query();
+		setStories(stories);
+	}, [stories]);
+	const onRemoveStory = async storyId => {
+		if (!stories) return;
+		await storyService.remove(storyId);
+		const updatedStories = stories.filter(story => story._id !== storyId) || null;
+		setStories(updatedStories);
+	};
+	useEffect(() => {
+		if (!stories) getStories(), [stories];
+	});
+	return stories ? (
+		<StoryList stories={stories} onRemoveStory={onRemoveStory} />
+	) : (
+		<Loading />
+	);
 };
 
 export default Home;
