a2acaa51f6cdef808bc3441b2a3e82edc16e92d8
BarBronshtein <barbronshtein@gmail.com>
2022-09-04 13:26:52 +0300

    added render template to story preview cmp

--- src/components/StoryPreview.tsx
+++ src/components/StoryPreview.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import AvatarPreview from './AvatarPreview';
 
 const StoryPreview = ({ story, onRemoveStory }) => {
 	return (
-		<article className="story-preview" onClick={() => onRemoveStory()}>
+		<article
+			className="story-preview story-layout"
+			onClick={() => onRemoveStory()}
+		>
 			<div className="flex justify-between">
 				<div className="flex">
-					<AvatarPreview user={story.by} size={''} />
+					<AvatarPreview user={story.by} size={'small'} />
+					<Link to={`/${story.by.fullname}`}>{story.by.fullname}</Link>
 				</div>
 				{/* Icon */}
+			</div>
+			<div className="img-container">
+				<img src={story.imgUrl} alt="" />
+			</div>
+			<div className="actions-icons flex ">
+				{/* Like-Icon */}
+				{/* Msg-Icon */}
+				{/* Share-Icon */}
 			</div>
 		</article>
 	);
